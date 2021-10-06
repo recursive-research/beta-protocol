81c6e1def1ce09d4c5f5f93fb5625953afe9ab72
ttarsi <tytarsi@gmail.com>
2021-10-05 22:56:33 -0400

    chore: remove hardhat/console from contract

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.6;
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import 'hardhat/console.sol';
 
 contract Vault is ERC20('RIFT - Fixed Rate ETH', 'frETH'), Ownable {
     uint256 public fixedRate;
