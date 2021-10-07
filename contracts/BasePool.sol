8b627fbe5b62581e593dc1e222e610a00258c8db
ttarsi <tytarsi@gmail.com>
2021-10-07 01:55:50 -0400

    chore: remove unecessary imports

--- contracts/BasePool.sol
+++ contracts/BasePool.sol
@@ -3,9 +3,6 @@ pragma solidity 0.8.6;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import './libraries/UniswapV2Library.sol';
-import './interfaces/IMasterChef.sol';
-import './interfaces/IUniswapV2Router02.sol';
 import './interfaces/IVault.sol';
 import './interfaces/IPool.sol';
 
--- contracts/PoolMasterChef.sol
+++ contracts/PoolMasterChef.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.6;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import './libraries/UniswapV2Library.sol';
 import './interfaces/IMasterChef.sol';
