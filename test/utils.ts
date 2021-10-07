a07589cd3eea67282cc3ec79bad15250be3a8ad3
ttarsi <tytarsi@gmail.com>
2021-10-07 01:45:51 -0400

    feat: deposit lp tokens to masterchef (1 or 2) on pairLiquidity

--- test/utils.ts
+++ test/utils.ts
@@ -3,9 +3,9 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { BigNumber } from 'ethers';
 import { Artifact } from 'hardhat/types';
 
-import { ERC20, Vault, Pool } from '../typechain';
+import { IMasterChef, ERC20, Vault, IPool, IMasterChefV2 } from '../typechain';
 import { deployContract } from 'ethereum-waffle';
-import { getWhale } from './constants';
+import { Contracts, getMasterChefPid, getPoolType, getWhale } from './constants';
 
 // Helper functions to deploy contracts
 export async function deployVault(admin: SignerWithAddress, fixedRate: BigNumber, maxEth: BigNumber): Promise<Vault> {
@@ -13,14 +13,27 @@ export async function deployVault(admin: SignerWithAddress, fixedRate: BigNumber
   return (await deployContract(admin, vaultArtifact, [fixedRate, maxEth])) as Vault;
 }
 
-export async function deployPool(admin: SignerWithAddress, vault: Vault, token: ERC20): Promise<Pool> {
-  const poolArtifact: Artifact = await hre.artifacts.readArtifact('Pool');
-  return (await deployContract(admin, poolArtifact, [vault.address, token.address])) as Pool;
+export async function deployPool(admin: SignerWithAddress, vault: Vault, token: ERC20): Promise<IPool> {
+  const poolType = getPoolType(token.address);
+  const poolArtifact: Artifact = await hre.artifacts.readArtifact(poolType);
+  return (await deployContract(admin, poolArtifact, [
+    vault.address,
+    token.address,
+    getMasterChefPid(token.address),
+  ])) as IPool;
 }
 
 // Helper function to get existing contracts
 export async function getERC20(address: string): Promise<ERC20> {
   return (await hre.ethers.getContractAt('ERC20', address)) as ERC20;
+}
+
+export async function getMasterChef(): Promise<IMasterChef> {
+  return (await hre.ethers.getContractAt('IMasterChef', Contracts.masterChef)) as IMasterChef;
+}
+
+export async function getMasterChefV2(): Promise<IMasterChefV2> {
+  return (await hre.ethers.getContractAt('IMasterChefV2', Contracts.masterChefV2)) as IMasterChefV2;
 }
 
 export async function getTokens(user: SignerWithAddress, token: ERC20, amount: BigNumber): Promise<void> {
@@ -36,4 +49,18 @@ async function impersonateAccount(address: string): Promise<void> {
     method: 'hardhat_impersonateAccount',
     params: [address],
   });
+}
+
+export async function mineBlock(): Promise<void> {
+  await hre.network.provider.request({
+    method: 'evm_mine',
+    params: [],
+  });
+}
+
+export async function mineBlocks(n: number): Promise<void> {
+  while (n > 0) {
+    await mineBlock();
+    n--;
+  }
 }
