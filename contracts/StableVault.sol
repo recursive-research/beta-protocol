7cafafb45c92610f58125d2b2287c2950d202d53
ttarsi <tytarsi@gmail.com>
2021-10-12 14:44:49 -0400

    docs: natspec comments for stablevault

--- contracts/StableVault.sol
+++ contracts/StableVault.sol
@@ -9,20 +9,26 @@ import './interfaces/IUniswapV2Router02.sol';
 import './libraries/UniswapV2Library.sol';
 import './StableVaultToken.sol';
 
+/// @title Rift V1 StableVault
+/// @notice allows users to Deposit USDC or USDT that will be deployed to a Uniswap Pool
 contract StableVault is Ownable {
     using SafeERC20 for IERC20;
 
+    /// @notice addresses for various contracts that the Pool will interact with
     address public constant uniswapFactory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
     address public constant uniswapRouter = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     address public constant usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public constant usdt = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
 
+    /// @notice state variables to track phases of the contract
     bool public liquidityAdded;
     bool public liquidityRemoved;
 
+    /// @notice ERC20s that will be minted and burned on USDC/USDC deposit/withdraw
     StableVaultToken public svUsdc = new StableVaultToken(usdc);
     StableVaultToken public svUsdt = new StableVaultToken(usdt);
 
+    /// @notice LP tokens for the USDC-USDT UniswapV2 Pool
     address public pair = UniswapV2Library.pairFor(uniswapFactory, usdc, usdt);
 
     /// @notice restricts actions to USDC and USDT
@@ -31,7 +37,9 @@ contract StableVault is Ownable {
         _;
     }
 
-    /// @notice allows users to deposit a token before liquidity has been deployed
+    /// @notice allows users to deposit a token before liquidity has been deployed. Mints
+    /// the user a StableVaultToken ERC20 based on which token they deposit
+    /// @param _token token to deposit - USDC or USDT
     /// @param _amount how much of the token to deposit, and how many staking tokens will be minted
     function depositToken(address _token, uint256 _amount) external validToken(_token) {
         require(!liquidityAdded, 'Liquidity already deployed');
@@ -39,6 +47,11 @@ contract StableVault is Ownable {
         _token == usdc ? svUsdc.mint(msg.sender, _amount) : svUsdt.mint(msg.sender, _amount);
     }
 
+    /// @notice allows user to withdraw or migrate from the StableVault at the end. Can only be withdrawn
+    /// after the Uniswap LP tokens have been withdraw and converted back to USDC/USDT
+    /// @param _token token to deposit - USDC or USDT
+    /// @param _poolV2 if the user wishes to migrate their tokens to Rift's V2 StableVaults,
+    /// they can do so by setting this parameter as a vaild V2 StableVault address
     function withdrawToken(address _token, address _stableVaultV2)
         external
         validToken(_token)
@@ -61,7 +74,12 @@ contract StableVault is Ownable {
         }
     }
 
-    /// @notice called by the owner after tokens have been deposited
+    /// @notice called by the owner after tokens have been deposited. Adds maximum amounts
+    /// of USDT and USDC to the UniswapV2 Pool
+    /// @param _minUsdc minimum amount of USDC to submit to the Pool. Set by the owner
+    /// to prevent frontrunning
+    /// @param _minUsdt minimum amount of USDT to submit to the Pool. Set by the owner
+    /// to prevent frontrunning
     function addLiquidity(uint256 _minUsdc, uint256 _minUsdt) external onlyOwner {
         require(!liquidityRemoved, 'Liquidity already removed');
         uint256 usdcBalance = IERC20(usdc).balanceOf(address(this));
@@ -83,7 +101,12 @@ contract StableVault is Ownable {
         liquidityAdded = true;
     }
 
-    /// @notice called by the owner after liquidity has been deployed
+    /// @notice called by the owner after liquidity has been deployed to remove liquidity
+    /// from the UniswapV2 pool
+    /// @param _minUsdc minimum amount of USDC to receive from the Pool. Set by the owner
+    /// to prevent frontrunning
+    /// @param _minUsdt minimum amount of USDT to receive from the Pool. Set by the owner
+    /// to prevent frontrunning
     function removeLiquidity(uint256 _minUsdc, uint256 _minUsdt) external onlyOwner {
         require(liquidityAdded, 'Liquidity not yet deployed');
         uint256 lpTokenBalance = IERC20(pair).balanceOf(address(this));
--- contracts/StableVaultToken.sol
+++ contracts/StableVaultToken.sol
@@ -4,7 +4,10 @@ pragma solidity 0.8.6;
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
+/// @title Rift V1 Stable Vault Token
+/// @notice deployed by the Stable Vault to mint staking tokens for USDC or USDT deposits
 contract StableVaultToken is ERC20, Ownable {
+    /// @notice address of underlying
     address public immutable token;
 
     constructor(address _token)
@@ -16,10 +19,12 @@ contract StableVaultToken is ERC20, Ownable {
         token = _token;
     }
 
+    /// @notice allows owner to mint StableVaultTokens
     function mint(address _user, uint256 _amount) external onlyOwner {
         _mint(_user, _amount);
     }
 
+    /// @notice allows owner to burn StableVaultTokens
     function burn(address _user, uint256 _amount) external onlyOwner {
         _burn(_user, _amount);
     }
