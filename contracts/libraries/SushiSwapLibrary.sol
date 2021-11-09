596d8921395a6ef3f0f5cfac15081c3d07dd6d75
Tyler Tarsi <tytarsi@gmail.com>
2021-11-09 17:17:28 -0500

    fix: order of operations in library

--- contracts/libraries/SushiSwapLibrary.sol
+++ contracts/libraries/SushiSwapLibrary.sol
@@ -54,7 +54,7 @@ library SushiSwapLibrary {
         require(amountOut > 0, 'UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT');
         require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
         uint256 numerator = reserveIn * amountOut * 1000;
-        uint256 denominator = reserveOut - amountOut * 997;
+        uint256 denominator = (reserveOut - amountOut) * 997;
         amountIn = numerator / denominator + 1;
     }
 }
