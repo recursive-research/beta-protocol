d9ba21755309c0c9941b7ddbb13523fddb717dfe
Tyler Tarsi <tytarsi@gmail.com>
2021-11-24 17:50:32 -0500

    big boy simulation before going live

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -47,7 +47,7 @@ const config: HardhatUserConfig = {
       chainId: 31337,
       forking: {
         url: alchemyApiUrlMainnet,
-        blockNumber: 13628310, // includes live rift contracts
+        blockNumber: 13665710, // includes live rift contracts
       },
     },
     mainnet: {
