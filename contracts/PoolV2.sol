4aa214bba545a196e3c46b369eb8e065b1f08872
Tyler Tarsi <tytarsi@gmail.com>
2021-12-14 13:47:55 -0500

    fix: import path

--- contracts/PoolV2.sol
+++ contracts/PoolV2.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.6;
 
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import '../interfaces/IPoolV2.sol';
+import './interfaces/IPoolV2.sol';
 
 contract PoolWithdraw is IPoolV2 {
     using SafeERC20 for IERC20;
--- contracts/VaultV2.sol
+++ contracts/VaultV2.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.6;
 
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import '../interfaces/IVaultV2.sol';
+import './interfaces/IVaultV2.sol';
 
 contract VaultWithdraw is IVaultV2 {
     using SafeERC20 for IERC20;
